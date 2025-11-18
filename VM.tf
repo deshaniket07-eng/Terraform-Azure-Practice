15241aa11ee40e029d84e707a8a859d6fce34a77
deshaniket07-eng <deshaniket07@gmail.com>
2025-11-18 17:18:21 +0100

    Add files via upload

--- VM.tf
+++ VM.tf
@@ -112,5 +112,8 @@ resource "azurerm_linux_virtual_machine" "vm" {
 
 }
 
-
+resource "azurerm_network_interface_security_group_association" "example" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
 
